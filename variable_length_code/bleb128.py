a614750a00f8945a47ff8fb3870bb99ffc197ffa
Uladzislau Khamkou <kham@tuta.io>
2022-01-25 00:20:09 +0300

    add signed bijective LEB128

--- variable_length_code/bleb128.py
+++ variable_length_code/bleb128.py
@@ -13,3 +13,14 @@ def bytes2uint(bts):
 
 assert list(uint2bytes(624485)) == [0xE5, 0x8D, 0x25]
 assert bytes2uint([1, 2, 3]) == 65921
+
+def int2bytes(n):
+    n, s = abs(n), (n < 0)
+    return uint2bytes((n - s) * 2 + s)
+
+def bytes2int(bts):
+    n, s = divmod(bytes2uint(bts), 2)
+    return (n + s) * (-1) ** s
+
+assert sum(int2bytes(36312488334073920)) == 1024
+assert bytes2int([3]) == -2

0f9aa24e6e4ed331f30331ca868c171ea37cb08c
Uladzislau Khamkou <kham@tuta.io>
2022-01-27 21:04:56 +0300

    make signed bijective LEB128 use two's complement
    The bleb.py used zigzag encoding to support signed BLEB128.
    This approach was inconsistent with the LEB128 one: two's complement.

--- variable_length_code/bleb128.py
+++ variable_length_code/bleb128.py
@@ -15,12 +15,20 @@ assert list(uint2bytes(624485)) == [0xE5, 0x8D, 0x25]
 assert bytes2uint([1, 2, 3]) == 65921
 
 def int2bytes(n):
-    n, s = abs(n), (n < 0)
-    return uint2bytes((n - s) * 2 + s)
+    result = []
+    while abs(2*n + 1) > 128:
+        n -= 64 * (-1) ** (n < 0)
+        result.append(n % 128 + 128)
+        n = n // 128
+    return bytearray(result + [n % 128])
 
 def bytes2int(bts):
-    n, s = divmod(bytes2uint(bts), 2)
-    return (n + s) * (-1) ** s
+    num = 0
+    for byte in reversed(bts):
+        num = num * 128 + (byte & 127)
+    num -= 2**(7 * len(bts)) * (bts[-1] & 64 > 0)
+    num += sum(64 * 128**p for p in range(len(bts) - 1)) * (-1) ** (num < 0)
+    return num
 
-assert sum(int2bytes(36312488334073920)) == 1024
-assert bytes2int([3]) == -2
+assert sum(int2bytes(8256)) == 256
+assert bytes2int([142, 42]) == 5454
